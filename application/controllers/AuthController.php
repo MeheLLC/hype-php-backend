03f564e31bcfb7e5dc5baff2deef5f8bae747444
Jamone Kelly <xg.monie089@gmail.com>
2017-07-09 16:55:11 -0400

    Base controller for auth

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -3,4 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class AuthController extends CI_Controller {
 	
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function signIn() {
+		
+	}
+	
+	public function signUp() {
+		
+	}
 }
